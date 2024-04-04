6e9308b23bc7bb303a64d03798a4b41e321fc7cd
Psmurf254 <135896532+Psmurf254@users.noreply.github.com>
2024-04-04 10:37:10 +0300

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,21 +1,37 @@
-from django.contrib import admin
-from . models import *
-
-
-class AppointmentAdmin(admin.ModelAdmin):
-    list_display = ('patient', 'specialist', 'date_time',  'payment_status', 'status')
-    search_fields = ['date_time']
-
-    
-
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('booking', 'amount', 'payment_method', 'transaction_id', 'timestamp', 'phone_number', 'payment_status')
-    list_filter = ('payment_status',)
-    search_fields = ('booking__patient__username', 'booking__specialist__full_name')
-
-admin.site.register(SpecialistCategory)
-admin.site.register(Specialist)
-admin.site.register(SpecialistFeedback)
-admin.site.register(Payment, PaymentAdmin)
-admin.site.register(Appointment, AppointmentAdmin)
-
+{
+"Body": 
+{
+    "stkCallback": 
+    {
+        "MerchantRequestID": "21605-295434-4",
+        "CheckoutRequestID": "ws_CO_04112017184930742",
+        "ResultCode": 0,
+        "ResultDesc": "The service request is processed successfully.",
+        "CallbackMetadata": 
+        {
+            "Item": 
+            [
+                {
+                    "Name": "Amount",
+                    "Value": 1
+                },
+                {
+                    "Name": "MpesaReceiptNumber",
+                    "Value": "LK451H35OP"
+                },
+                {
+                    "Name": "Balance"
+                },
+                {
+                    "Name": "TransactionDate",
+                    "Value": 20171104184944
+                },
+                {
+                    "Name": "PhoneNumber",
+                    "Value": 254114272921
+                }
+            ]
+        }
+    }
+}
+}
